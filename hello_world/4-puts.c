e051972e2bd00f1fed92fc1a748df30dd0342b00
santiago2221212 <santihouseman@gmail.com>
2025-02-19 12:51:39 +0000

    puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+* main - principal funtion
+* Return: return 0 if the program runs without problems
+*/
+int main(void)
+{
+	/*puts prints on screen the following text*/
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
